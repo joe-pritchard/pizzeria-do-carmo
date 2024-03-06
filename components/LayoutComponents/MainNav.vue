7154dee809bb0be4f6ba17f2bea5c013fee457c8
Joe Pritchard <joe@var.digital>
2024-03-06 14:06:42 +0000

    Also remove the booking button from desktop nav if link not set

--- components/LayoutComponents/MainNav.vue
+++ components/LayoutComponents/MainNav.vue
@@ -26,6 +26,7 @@
                             Our Menu
                         </a>
                         <button
+                            v-if="siteConfig.bookingLink"
                             class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium"
                             :class="{
                                 'text-white hover:border-gray-100 hover:text-gray-300': isTopOfPage,
